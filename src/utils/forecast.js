724a83bd9a667371670c185fc7d9e5be9edcfbd8
Miguel Guzman <53159738+mag-projects@users.noreply.github.com>
2020-01-08 19:50:51 -0800

    Added tomorrow's max high temp and summary for the day

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,7 +5,8 @@ const forecast = (lat, long, callback) => {
     request({url, json: true}, (err, {body}) => {
         if (err) callback(`DarkSky weather services could not be reached, please try again`, undefined);
         else if (body.error) callback(`Weather information for the specified location could not be found.`, undefined);
-        else callback(undefined, `${body.currently.summary} with a current temperature of ${body.currently.temperature}.\nCurrently there is a ${body.currently.precipProbability}% chance of rain.`)
+        else callback(undefined, `${body.currently.summary} with a current temperature of ${body.currently.temperature}. Currently there is a ${body.currently.precipProbability}% chance of rain. Tomorrow: 
+         ${body.daily.data[0].temperatureMax} degrees. ${body.daily.data[0].summary}`)
     })
 };
 
